fd9a7fded84e42f95725b37f893db8e4f6cfba98
israt <israt9528@gmail.com>
2025-12-18 19:47:37 +0600

    add vercel.json file

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ app.use(cors());
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebase-adminsdk.json");
+// const serviceAccount = require("./firebase-adminsdk.json");
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf8"
+);
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -364,10 +369,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
